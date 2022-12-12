192180aed8341ebcd53ded9f9848f4784d05fdf3
lucazin <bergamo86@gmail.com>
2022-12-12 19:48:33 -0300

    DFS

---
+++ Java Practice/src/MaximumSubArray.java
@@ -0,0 +1,50 @@
+public class MaximumSubArray {
+    
+/* 
+ * 
+ * Given a non-empty array of integers arr, create a function that returns the sum of the subarray that has the greatest sum.
+We don't consider the empty array [] as a subarray.
+
+Example 1:
+
+Input: arr = [2, 3, -6, 4, 2, -8, 3]
+
+Output: 6
+
+Explanation: the maximum subarray is [4, 2], its sum is 6
+
+ */
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        int[] arr = {2, 3, -6, 4, 2, -8, 3};
+       System.out.println( Maximum(arr));
+
+    }
+
+    public static int Maximum(int[] arr)
+    {
+        //O(n)
+
+        int globalsum = 0;  // Variavel glocal pra somar com a soma local.
+        int localsum = 0; //variavel para somar os itens do array e retornar a maior soma
+
+        for(int i =0;i<=arr.length-1;i++)
+        {// faco a iteracao da lista
+
+            //qual e maior 2 ou 0 ? retorna 2 pro local sum
+            //qual e maior 2 ou 2+3 = 5 ? retorna 5 pro local sum
+            //qual e maior -6 ou 5 + -6 = 1 = retorna 1
+            //qual e maior 4 ou 1 +  4 = 5 // retorna 5 
+            // . . .. . . . . . 
+            localsum = Math.max(arr[i], localsum + arr[i]);  // aqui eu retorno a soma de cada item do array somando com a soma local mais item do array
+          
+            //incremento o localsum com o global sum e vejo qual e o maior no retorno do math.max
+            globalsum = Math.max(globalsum, localsum); // faco a soma e retorno qual maior soma 
+        }
+
+        return globalsum;
+    }
+    
+}
---
+++ Java Practice/src/ThreeDepthFirstSearch_DFS.java
@@ -0,0 +1,157 @@
+public class ThreeDepthFirstSearch_DFS {
+
+    /* 
+     *    Given a binary tree of integers root, create 3 functions to print the tree nodes in preorder, inorder, and postorder traversal.
+Preorder: print the root value, then print the left subtree, then print the right subtree.
+Inorder: print the left subtree, then print the root value, then print the right subtree.
+Postorder: print the left subtree, then print the right subtree, then print the root value.
+
+Example 1:
+
+Input: root = [1, 2, 3, 4, 5, 6, 7]
+
+Output:
+Preorder: 1 2 4 5 3 6 7
+Inorder: 4 2 5 1 6 3 7
+Postorder: 4 5 2 6 7 3 1
+
+     * 
+     *  
+     */
+    
+    static class Tree{
+        int data;
+        Tree left;
+        Tree right;
+        Tree(int data){this.data = data; this.left = null; this.right = null;}
+        Tree(int data, Tree left, Tree right){this.data = data; this.left = left; this.right = right;}
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+
+        //Input: root = [1, 2, 3, 4, 5, 6, 7]
+
+        /* 
+                        1 
+                    2      3
+                  4   5   6  7
+          
+           1 (root)  2 (root left) 3 (root right) 4,5 (left children) 6,7 (right child)
+         
+         */
+
+
+         // DFS Search em uma arvore nos 3 modos  PreOrder, InOrder e PostOrder
+
+        Tree tree = new Tree(1);
+
+        tree.left = new Tree(2);
+        tree.left.left = new Tree(4);
+        tree.left.right = new Tree(5);
+
+        tree.right = new Tree(3);
+        tree.right.left = new Tree(6);
+        tree.right.right = new Tree(7);
+
+        System.out.println("PreOrder #####");
+        dfs_Preorder(tree);
+        System.out.println("InOrder #####");
+        dfs_inOrder(tree);
+        System.out.println("PostOrder #####");
+        dfs_PostOrder(tree);
+
+    }
+
+    public static void dfs_Preorder(Tree root)
+    {
+         //Original
+        /* 
+                        1 
+                    2      3
+                  4   5   6  7 
+                  
+                  
+            print 1 root
+            print 2 - 4 - 5 (Left)
+            print 3 - 6- 7
+         */
+
+        //Preorder: 1 2 4 5 3 6 7
+        //Print root 
+        //Print left subtree
+        //Print right subtree
+
+        if(root == null)return;
+        System.out.println(root.data);
+        dfs_Preorder(root.left);
+        dfs_Preorder(root.right);
+
+
+    }
+
+    public static void dfs_inOrder(Tree root)
+    {
+
+        // de baixo pra cima - Esqueda - root - Direita
+        //Original
+        /* 
+                        Root
+                         1
+
+                  Left      Right
+                          
+                    2         3
+                  4   5      6  7   
+                  
+                 
+            Print 4 - 2 - 5
+                   2
+                4    5
+            
+                Print root - 1  
+
+             Print 6 - 3 - 7
+                   3
+                6    7
+         */
+        
+         
+
+
+        //Inorder: 4 2 5 1 6 3 7 
+        //Print leftsubtree
+        //print root
+        //print rightsubtree  
+
+        if(root == null)return;
+        dfs_inOrder(root.left);
+        System.out.println(root.data);
+        dfs_inOrder(root.right);
+    }
+
+    public static void dfs_PostOrder(Tree root)
+    {
+        
+         //Original
+        /* 
+                        1 
+                    2      3
+                  4   5   6  7    
+         */
+        
+
+        //Postorder: 4 5 2 6 7 3 1
+        //Print left subtree
+        //Print rightsubtree
+        //Print root
+
+        if(root == null)return;
+        dfs_PostOrder(root.left);
+        dfs_PostOrder(root.right);
+        System.out.println(root.data);
+    }
+
+
+}
